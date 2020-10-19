8c63d430c943a04ed5928440cd34c39b0ba415df
Windmill-City <1449182174@qq.com>
2020-10-19 13:21:58 +0800

    Add task depends impl processModel rearranged code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 import org.ajoberstar.grgit.Grgit
-import org.eclipse.jgit.submodule.SubmoduleStatusType.UNINITIALIZED
+import java.nio.file.Files
 import java.nio.file.Path
-import java.nio.file.Paths.get
 
 plugins{
     id("org.hidetake.swagger.generator") version "2.18.2"
@@ -16,57 +15,85 @@ repositories {
 }
 
 dependencies {
-    swaggerCodegen ("io.swagger:swagger-codegen-cli:2.4.+")
+    swaggerCodegen("io.swagger:swagger-codegen-cli:2.4.+")
 }
 
 val urlModel = "https://github.com/amzn/selling-partner-api-models.git"
-val dirModel: Path = get("selling-partner-api-models")
 val groupName = "sp-api"
+//Model-Dir
+val dirModel: Path = projectDir.toPath().resolve("selling-partner-api-models")
+//Client
+val clientJava: Path = dirModel.resolve("clients/sellingpartner-api-aa-java")
+val clientTemplates: Path = clientJava.resolve("resources/swagger-codegen/templates")
+val clientSrc: Path = clientJava.resolve("src")
+val clientTest: Path = clientJava.resolve("tst")
+//Model
+val models: Path = dirModel.resolve("models")
+val modelRegex: Regex = Regex("""^([a-zA-Z]+)(?:V0|_[\-0-9]+)?\.json$""")
 
 task("initModel") {
     group = groupName
-    doLast{
-        val grgit = Grgit.open{ dir = projectDir }
+    outputs.dir(dirModel)
+    doLast {
+        val grgit = Grgit.open { dir = projectDir }
         val jgit = grgit.repository.jgit
-        val status = jgit.submoduleStatus().call()[dirModel.toString()]
-        status ?: throw Exception("Submodule -> Model not exist")
-        if(status.type == UNINITIALIZED) {
-            logger.info("Initialing submodules")
-            jgit.submoduleInit().call().forEach(logger::info)
-        }
-        logger.info("Updating submodules")
+        println("Initialing submodules")
+        jgit.submoduleInit().call().forEach(logger::info)
+        println("Updating submodules")
         jgit.submoduleUpdate().call().forEach(logger::info)
         grgit.close()
         //Checkout main
-        val repo = Grgit.open { currentDir=projectDir.toPath().resolve(dirModel) }
-        repo.checkout{ branch="main" }
+        val repo = Grgit.open { currentDir = projectDir.toPath().resolve(dirModel) }
+        repo.checkout { branch = "main" }
         repo.close()
     }
 }
 
 task("updateModel"){
     group = groupName
-    doLast{
-        val repo = Grgit.open { currentDir=projectDir.toPath().resolve(dirModel) }
+    dependsOn("initModel")
+    doLast {
+        val repo = Grgit.open { currentDir = dirModel }
         repo.pull()
         repo.close()
-        val mainRepo = Grgit.open{ dir = projectDir }
+        val mainRepo = Grgit.open { dir = projectDir }
         val status = mainRepo.status()
-        if (status.staged.modified.contains(".gitmodules") || status.unstaged.modified.contains(".gitmodules"))
+        if (status.staged.modified.contains(".gitmodules") || status.unstaged.modified.contains(".gitmodules")) {
+            println("Committing Model update")
             mainRepo.commit {
                 message = "Update Model"
                 paths = setOf(".gitmodules")
             }
+        }
         mainRepo.close()
     }
 }
 
-task("processModel"){
-    group=groupName
-    doLast{
-    }
+task("gen-All") {
+    group = groupName
 }
 
-task("genApiCode"){
+task("processModel") {
     group = groupName
+    inputs.dir(models)
+    dependsOn("initModel")
+    doLast {
+        var i = 0
+        Files.walk(models).forEach {
+            val matchResult = modelRegex.matchEntire(it.fileName.toString())
+            if (matchResult != null) {
+                i++
+                val taskName = matchResult.groups[1]!!.value
+                project.tasks.register("gen-${taskName}") {
+                    group = groupName
+                    inputs.file(it)
+                    tasks["gen-All"].dependsOn(taskName)
+                    doLast {
+                        println(it)
+                    }
+                }
+            }
+        }
+        println("Generated $i tasks")
+    }
 }
